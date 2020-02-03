ed8ff0dbea29cadd074990b76cae7470f7eba7c6
dima <dima.sivov@huawei.com>
2020-02-03 23:31:04 +0800

    Refactoring

--- cubicIndex/cIndex.cpp
+++ cubicIndex/cIndex.cpp
@@ -69,17 +69,67 @@ void process_mem_usage(double& vm_usage, double& resident_set)
    resident_set = rss * page_size_kb;
 }
 
-
-
-
-
-
 double fRand(double fMin, double fMax) {
     double f = (double) rand() / RAND_MAX;
     return fMin + f * (fMax - fMin);
 }
 
-int numberOfKeys = 100000000;
+void monoCubicIndex (std::vector<double> index, std::vector<double> positions, int numberOfKeys) {
+    int count = 0;
+    double _position;
+    double position;
+    double key;   
+    //gsl init
+    double vm, rss;
+    double vm1, rss1;
+    int64_t total_duration = 0;
+    int64_t std_total_duration = 0;
+    process_mem_usage(vm, rss);
+    cout << "VM Size before set point(Kb): " << (int64_t)vm << "; RSS(Kb): " << (int64_t)rss << endl;
+    process_mem_usage(vm1, rss1);
+    cout << "VM Size after set point(Kb): " << (int64_t)vm1 << "; RSS(Kb): " << (int64_t) rss1 << endl;
+    cout << "memory usage by function(Kb): " << (int64_t)vm1 - (int64_t)vm  << "; RSS (Kb): " << (int64_t) rss1 - (int64_t)rss<< endl;
+    cout << positions.size() << " " << index.size() << endl;
+    sort(index.begin(), index.end());
+    gsl_interp_accel *acc = gsl_interp_accel_alloc();
+    gsl_spline *spline_steffen = gsl_spline_alloc(gsl_interp_steffen, numberOfKeys);
+    gsl_spline_init(spline_steffen, &index[0], &positions[0], numberOfKeys); 
+    cout << positions.size() << " " << index.size() << endl;
+    for (size_t i = 0; i < index.size(); i++){  
+        auto start = chrono::high_resolution_clock::now(); 
+        //double position = s(index[i]);
+        //double position = alglib::spline1dcalc(spline, index[i]);
+        int res = gsl_spline_eval_e(spline_steffen, index[i], acc, &_position);
+        position =(size_t)(_position*index.size());
+        if (position != i){
+            cout <<"For real key:" << (int64_t)index[i] << " on position: " << i
+                    << " we found: " << (int64_t)key << " on position:" << position << endl;
+            cout << " Near keys, from right:" << (int64_t)index[i+1] << " Position:" << i+1
+                    << " from left:" << (int64_t)index[i-1] << " Position:" << i-1 << endl; 
+        }
+            
+        key = index[position];
+        auto stop = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::nanoseconds>(stop - start);
+        total_duration = total_duration + duration.count();
+        count++; 
+//        if (index[i] != key) {
+//            cout << "================================ERROR===========================" << endl;
+//            cout <<"For real key:" << (int64_t)index[i] << " on position: " << i
+//                    << " we found: " << (int64_t)key << " on position:" << position<< endl;
+//            cout << " Near keys, from right:" << (int64_t)index[i+1] << " Position:" << i+1
+//                    << " from left:" << (int64_t)index[i-1] << " Position:" << i-1 << endl; 
+//            cout << "===============================================================" << endl;
+//        }    
+        //cout << "Random Key:" << (int64_t)key << ";" << (int64_t)index[i] << " Real Position:" << i << " Predicted position:"
+         //       << position*index.size() << " Duration:" << duration.count() << endl;
+            
+    }
+    std::cout << "Average get: " << total_duration/count << " Number of gets:" << count << std::endl;
+    return;
+}
+
+int numberOfKeys = 10000000;
 int main(int argc, char** argv) {
     std::vector<double> index(numberOfKeys);
     std::vector<double> positions(numberOfKeys);
@@ -93,66 +143,8 @@ int main(int argc, char** argv) {
         positions[i] = double (i) / index.size();
     }
 
-
-    sort(index.begin(), index.end());
-
-    //tk::spline s;
-    
-    double vm, rss;
-    double vm1, rss1;
-    process_mem_usage(vm, rss);
-    cout << "VM Size before set point(Kb): " << (int64_t)vm << "; RSS(Kb): " << (int64_t)rss << endl;
-   
-    
-    process_mem_usage(vm1, rss1);
-    cout << "VM Size after set point(Kb): " << (int64_t)vm1 << "; RSS(Kb): " << (int64_t) rss1 << endl;
-    
-     cout << "memory usage by function(Kb): " << (int64_t)vm1 - (int64_t)vm  << "; RSS (Kb): " << (int64_t) rss1 - (int64_t)rss<< endl;
-    
-    cout << positions.size() << " " << index.size() << endl;
-    int64_t total_duration = 0;
-    int64_t std_total_duration = 0;
-    int count = 0;
-    double position;
-    double key;
-
-    
-    //gsl init
-    gsl_interp_accel *acc = gsl_interp_accel_alloc();
-    gsl_spline *spline_steffen = gsl_spline_alloc(gsl_interp_steffen, numberOfKeys);
-    gsl_spline_init(spline_steffen, &index[0], &positions[0], numberOfKeys); 
-    //alglib::real_1d_array AX, AY;
-    //AX.setcontent(index.size(), &(index[0]));
-    //AY.setcontent(positions.size(), &(positions[0]));
-    //alglib::spline1dinterpolant spline;
-    //alglib::spline1dbuildlinear(AX, AY, index.size(), 2,0.0,2,0.0, spline);
-    //alglib::spline1dbuildlinear(AX, AY, index.size(),spline);
-    //tk::spline s;
-    //s.set_boundary(tk::spline::second_deriv,0,tk::spline::first_deriv,400,false);
-    //s.set_points(index,positions);
-    cout << positions.size() << " " << index.size() << endl;
-    for (size_t i = 0; i < index.size(); ++i){  
-        auto start = chrono::high_resolution_clock::now(); 
-        //double position = s(index[i]);
-        //double position = alglib::spline1dcalc(spline, index[i]);
-        position = gsl_spline_eval(spline_steffen, index[i], acc);
-        key = index[position*index.size()];
-        auto stop = chrono::high_resolution_clock::now();
-        auto duration = chrono::duration_cast<chrono::nanoseconds>(stop - start);
-        total_duration = total_duration + duration.count();
-        count++;         
-        //cout << "Random Key:" << (int64_t)key << ";" << (int64_t)index[i] << " Real Position:" << i << " Predicted position:"
-         //       << position*index.size() << " Duration:" << duration.count() << endl;
-            
-    }
-    std::cout << "Average get: " << total_duration/count << " Number of gets:" << count << std::endl;   
-//    double position = alglib::spline1dcalc(spline,index[5003]);
-//    double st = (index[5003] - index[5002])/2;
-//    double stored_position = alglib::spline1dcalc(spline,index[5003] - st);
-//    cout << "ALGLIB " << "Stored Min:" << (int64_t)index[5002] << " Stored Max:" << (int64_t)index[5003] << " Predict this Key:" 
-//            << (int64_t)(index[5003] - st) << " Near stored position:"
-//              << (double)position*index.size() << " Predicted position:" << (double)stored_position*index.size() << " Real key:" 
-//            << (int64_t)index[(int)(stored_position*index.size())]<< endl;
+    //GSL Lib monotonic cubic spline
+    monoCubicIndex (index, positions, numberOfKeys);
 
    /*
     key = 1680000000000000;
